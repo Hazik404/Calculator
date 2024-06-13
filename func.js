a83377fa6fd64e38eed988f35f4533478668fe5c
Hazik404 <76013111+Hazik404@users.noreply.github.com>
2024-06-13 11:42:46 +0500

    basic operators and boilerplate for html

---
+++ func.js
@@ -0,0 +1,20 @@
+function add(a,b){
+    return a + b
+}
+
+function sub(a,b){
+    return b - a
+}
+
+function multi(a,b){
+    return a * b
+}
+
+function divide(a,b){
+    return a / b
+}
+
+console.log(add(2,3))
+console.log(sub(2,3))
+console.log(multi(2,3))
+console.log(divide(2,3))
