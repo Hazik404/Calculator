47a0e40d5cf5ef61640a9da35c7399dc4235ba33
Hazik404 <76013111+Hazik404@users.noreply.github.com>
2024-06-13 16:02:55 +0500

    setup basic calculator layout and basic function later to wired and styled them

--- func.js
+++ func.js
@@ -14,6 +14,13 @@ function divide(a,b){
     return a / b
 }
 
+const x = '';
+const y = '';
+const operator = '';
+
+function operate(x,y,operator){
+    return (x) (operator) (y);
+}
 console.log(add(2,3))
 console.log(sub(2,3))
 console.log(multi(2,3))
