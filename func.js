1a7bf67d03fccc10443f431c3045a38b316ba2a6
Hazik404 <76013111+Hazik404@users.noreply.github.com>
2024-06-14 12:22:07 +0500

    numeric keys and equal are no working, having wired them using js

--- func.js
+++ func.js
@@ -1,3 +1,23 @@
+let displayValue = '';
+const display = document.getElementsByClassName('display')['0'];
+
+function updateDisplay(){
+    display.textContent = displayValue;
+}
+const numKeys = document.getElementsByClassName('numkey');
+
+for(let button of numKeys){
+    button.addEventListener('click', function() {
+        const buttonValue = button.textContent;
+
+        if(displayValue === '0'){
+            displayValue = buttonValue
+        }else{
+            displayValue += buttonValue;
+        }
+        updateDisplay();
+    });
+}
 function add(a,b){
     return a + b
 }
@@ -14,13 +34,22 @@ function divide(a,b){
     return a / b
 }
 
-const x = '';
-const y = '';
-const operator = '';
+let x = '';
+let y = '';
+let operator = '';
 
 function operate(x,y,operator){
     return (x) (operator) (y);
 }
+
+const clr = document.getElementsByClassName('clear')['0'];
+clr.addEventListener('click',function(){
+    displayValue = '0';
+    x = '';
+    y = '';
+    operator = '';
+    updateDisplay();
+});
 console.log(add(2,3))
 console.log(sub(2,3))
 console.log(multi(2,3))
